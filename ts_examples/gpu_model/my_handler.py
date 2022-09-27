e44c4c8b2129b5365266cbae4c7ad04de1c6613f
Mookyung Song <moono.song@ncsoft.com>
2022-09-27 06:04:43 +0000

    use multiple type input

--- ts_examples/gpu_model/my_handler.py
+++ ts_examples/gpu_model/my_handler.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from io import BytesIO
 from logging.config import dictConfig
-from typing import List, Dict, Any, Tuple
+from typing import List, Dict, Any, Tuple, Union
 from zipfile import ZipFile
 from PIL import Image
 from ts.context import Context
@@ -71,7 +71,9 @@ class MyTSModel(object):
         logger.info(f"Model initialization ... DONE !!")
         return
 
-    def preprocess(self, data: List[Dict[str, Any]]) -> List[str]:
+    def preprocess(
+        self, data: List[Dict[str, Any]]
+    ) -> List[Dict[str, Union[str, Image.Image, None]]]:
         def _print(item: Any):
             for k, v in item.items():
                 if isinstance(v, dict):
@@ -79,25 +81,64 @@ class MyTSModel(object):
                 else:
                     logger.debug(f"{k}: {type(v)}")
             return
+
         inputs = list()
         for row in data:
             _print(row)
-            inputs.append("inputs ...")
+
+            txt = row.get("txt", None)
+            img = row.get("img", None)
+
+            assert isinstance(txt, str)
+            logger.debug(f"input txt: {txt}")
+            if img is not None:
+                assert isinstance(img, bytearray)
+                img = Image.open(BytesIO(img)).convert("RGB")
+                logger.debug(f"input img size: {img.size}")
+
+            inputs.append({"txt": txt, "img": img})
         return inputs
 
-    def inference(self, data: List[str]) -> List[str]:
+    def inference(
+        self, data: List[Dict[str, Union[str, Image.Image, None]]]
+    ) -> List[Dict[str, Union[str, Image.Image, None]]]:
         batch_size = len(data)
-        logger.debug(f"batch_size: {batch_size}")
+        # logger.debug(f"batch_size: {batch_size}")
+        for item in data:
+            txt = item["txt"]
+            img = item["img"]
+            if img is None:
+                logger.debug(f"running txt2img - {txt}")
+            else:
+                logger.debug(f"running img2img - {txt}")
         return data
 
-    def postprocess(data: List[str]) -> List[str]:
+    def postprocess(
+        self, data: List[Dict[str, Union[str, Image.Image, None]]]
+    ) -> List[bytes]:
         # image_bytes = list()
         # for img in images:
         #     buf = BytesIO()
         #     img.save(buf, format="JPEG")
         #     image_bytes.append(buf.getvalue())
         #     buf.close()
-        return data
+
+        outputs = list()
+        for item in data:
+            txt = item["txt"]
+            img = item["img"]
+            if img is None:
+                out = np.ones((512, 512, 3), dtype=np.uint8)
+                out = Image.fromarray(out)
+            else:
+                out = img
+
+            buf = BytesIO()
+            out.save(buf, format="JPEG")
+            outputs.append(buf.getvalue())
+            buf.close()
+
+        return outputs
 
     # must have: Torchserve
     def handle(self, data: List[Dict[str, Any]], context: Context):
@@ -112,8 +153,7 @@ class MyTSModel(object):
         logger.info(f"Received batch size of {batch_size} !!!")
 
         data1 = self.preprocess(data)
-        # data2 = self.inference(data1)
-        # output = self.postprocess(data2)
-        # logger.debug(f"output type: {type(output)}")
-        # return output
-        return [{"output": "output_sample"} for _ in range(batch_size)]
+        data2 = self.inference(data1)
+        output = self.postprocess(data2)
+        return output
+        # return [{"output": "output_sample"} for _ in range(batch_size)]
